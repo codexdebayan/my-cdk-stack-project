2c79560e2f6850a82d3dd355077393b85183de8f
Nitheesh Kumar Reddy <nitheeshkumarreddy5177@gmail.com>
2024-11-21 10:42:17 +0530

    Added: My-cdk_project

--- my_cdk_project/my_cdk_project_stack.py
+++ my_cdk_project/my_cdk_project_stack.py
@@ -1,19 +1,50 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    core as cdk,
+    aws_s3 as s3,
+    aws_kms as kms
 )
-from constructs import Construct
 
-class MyCdkProjectStack(Stack):
+class S3BucketStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+        # Create a KMS key for encryption
+        kms_key = kms.Key(
+            self, "BucketKey",
+            enable_key_rotation=True
+        )
 
-        # The code that defines your stack goes here
+        # Create the S3 bucket
+        bucket = s3.Bucket(
+            self, "YourBucketLogicalID",
+            # Bucket Name : 'mybucketcreationfromthecdk-6203'
+            bucket_name="mybucketcreationfromthecdk-6203",
+            versioned=True,
+            encryption=s3.BucketEncryption.KMS,
+            encryption_key=kms_key,
+            lifecycle_rules=[
+                s3.LifecycleRule(
+                    transitions=[
+                        s3.Transition(
+                            storage_class=s3.StorageClass.INFREQUENT_ACCESS,
+                            transition_after=cdk.Duration.days(30)
+                        ),
+                        s3.Transition(
+                            storage_class=s3.StorageClass.GLACIER,
+                            transition_after=cdk.Duration.days(90)
+                        )
+                    ]
+                )
+            ]
+        )
+        
+        cdk.CfnOutput(
+            self, "BucketName",
+            value=bucket.bucket_name
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "MyCdkProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+
+# Define the app
+app = cdk.App()
+S3BucketStack(app, "S3BucketStack")
+app.synth()
